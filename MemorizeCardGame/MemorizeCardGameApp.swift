d4c27daad52c2e567550ae17e952cca883fedb20
Mahjabin Alam <mahjabin.alam@rakuten.com>
2023-02-22 10:55:14 +0900

    initial Commit

---
+++ MemorizeCardGame/MemorizeCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeCardGameApp.swift
+//  MemorizeCardGame
+//
+//  Created by Alam, Mahjabin | Natasha | ECMPD on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
